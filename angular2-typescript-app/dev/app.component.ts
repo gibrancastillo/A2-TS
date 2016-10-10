e98c8a36982b3b35df0f84b8423f6891229495d9
Gibran Castillo <gibran-castillo@hotmail.com>
2016-10-10 16:49:00 -0400

    Playing with Angular 2 and TypeScript

---
+++ angular2-typescript-app/dev/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 & TypeScript Boilerplate</h1>
+        <p>Hello Angular 2 & TypeScript World!!</p>
+       
+    `,
+})
+export class AppComponent {
+
+}
